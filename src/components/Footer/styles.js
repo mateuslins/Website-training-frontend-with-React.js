7295b93186e6656046bce37a19e47e8f44e4b9c5
mateuslins <41246021+mateuslins@users.noreply.github.com>
2020-08-03 17:55:48 -0300

    Creating the footer

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 50px;
+    background: #12126D;
+    border-top: 5px solid #FF8000;
+    
+    display: flex;
+    position: fixed;
+    bottom: 0;
+
+    align-items: center;
+    justify-content: center;
+
+    span{
+        color: white;
+    }
+`;
