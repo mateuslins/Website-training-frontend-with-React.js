6ddd42b62c3f07843ebed7aad60935b655eb3ee8
mateuslins <41246021+mateuslins@users.noreply.github.com>
2020-08-14 22:07:48 -0300

    Registering new task

--- src/views/Task/index.js
+++ src/views/Task/index.js
@@ -14,17 +14,37 @@ function Task() {
 
     const [lateCount, setLateCount] = useState();
     const [type, setType] = useState();
+    const [id, setId] = useState();
+    const [done, setDone] = useState(false);
+    const [title, setTitle] = useState();
+    const [description, setDescription] = useState();
+    const [date, setDate] = useState();
+    const [hour, setHour] = useState();
+    const [macaddress, setMacaddress] = useState('11:11:11:11:11:11');
 
     async function lateVerify() {
         await api.get(`/task/filter/late/11:11:11:11:11:11`)
           .then(response => {
             setLateCount(response.data.length)
           })
-      }
+    }
 
-      useEffect(() => {
+    async function Save() {
+        await api.post('/task', {
+            macaddress,
+            type,
+            title,
+            description,
+            when: `${date}T${hour}:00.000`
+        })
+            .then(() => {
+                alert("TAREFA CADASTRADA COM SUCESSO");
+            });
+    }
+
+    useEffect(() => {
         lateVerify();
-      }, []);
+    }, []);
     
     return (
         <S.Container>
@@ -37,7 +57,7 @@ function Task() {
                             index > 0 && //&& serve como if já q é uma verificação simples
                             <button type="button" onClick={ () => setType(index) }>
                                 <img src={icon} alt="Tipo da tarefa" 
-                                className={ type && type != index && "inative" } /*se existir type e for diferente do index (escolhido), a classe é inative *//> 
+                                className={ (type && type != index) ? "inative" : "active" } /*se existir type e for diferente do index (escolhido), a classe é inative *//> 
                             </button>
                         ))
                     }
@@ -45,34 +65,34 @@ function Task() {
 
                 <S.Input>
                     <span>Título</span>
-                    <input type="text" placeholder="Título da tarefa"></input>
+                    <input type="text" placeholder="Título da tarefa" onChange={e => setTitle(e.target.value)} value={title} />
                 </S.Input>
 
                 <S.TextArea>
                     <span>Descrição</span>
-                    <textarea rows={5} placeholder="Detalhes da tarefa"></textarea>
+                    <textarea rows={5} placeholder="Detalhes da tarefa" onChange={e => setDescription(e.target.value)} value={description} />
                 </S.TextArea>
 
                 <S.Input>
                     <span>Data</span>
-                    <input type="date" placeholder="Data da tarefa"></input>
+                    <input type="date" placeholder="Data da tarefa" onChange={e => setDate(e.target.value)} value={date} />
                 </S.Input>
 
                 <S.Input>
                     <span>Hora</span>
-                    <input type="time" placeholder="Hora da tarefa"></input>
+                    <input type="time" placeholder="Hora da tarefa" onChange={e => setHour(e.target.value)} value={hour} />
                 </S.Input>
 
                 <S.Options>
                     <div>
-                        <input type="checkbox"></input>
+                        <input type="checkbox" checked={done} onChange={() => setDone(!done)} />
                         <span>CONCLUÍDO</span>
                     </div>
                     <button type="button">EXCLUIR</button>
                 </S.Options>
 
                 <S.Save>
-                    <button type="button">SALVAR</button>
+                    <button type="button" onClick={Save} >SALVAR</button>
                 </S.Save>
             </S.Form>
 
